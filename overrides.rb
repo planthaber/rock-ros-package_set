3f4379c88feff94ba705bfb937e42c50f79fb17f
Steffen Planthaber <Steffen.Planthaber@dfki.de>
2019-04-09 11:48:53 +0200

    set CMAKE_PREFIX_PATH for orogen components

--- overrides.rb
+++ overrides.rb
@@ -12,8 +12,8 @@
 # See config.yml to set the prefix:/opt/autoproj/2.0 globally for all packages.
 
 Autoproj.manifest.each_autobuild_package do |pkg|
-    next if !pkg.kind_of?(Autobuild::Orogen)
-    pkg.depends_on 'tools/rtt_transports/ros'
-    pkg.depends_on 'tools/orogen_metadata'
-    #pkg.define "CMAKE_PREFIX_PATH", Autoproj.user_config('ROS_PREFIX') + ";" + ENV["AUTOPROJ_CURRENT_ROOT"] + "/install"
+    if pkg.kind_of?(Autobuild::Orogen) 
+        pkg.depends_on 'tools/rtt_transports/ros'
+        pkg.define "CMAKE_PREFIX_PATH", Autoproj.user_config('ROS_PREFIX') + ";" + ENV["AUTOPROJ_CURRENT_ROOT"] + "/install"
+    end
 end
